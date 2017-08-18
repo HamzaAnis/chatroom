e9d56a7a9fa44115e52d7e96279c305780059d46
Hamza Anis <hamzaanis9514@gmail.com>
2017-08-18 13:52:31 -0700

    Room changing

--- public/chat.js
+++ public/chat.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-  // alert(sessionStorage.SessionName);  
+  // alert(sessionStorage.SessionName);
   document.getElementById('name').innerText = sessionStorage.SessionName;
   document.getElementById('time').innerText = "Online for --:-- minutes";
 
@@ -7,7 +7,7 @@ $(document).ready(function () {
     var div = document.getElementById('rooms');
 
     $.each(data, function (key, val) {
-      div.innerHTML += '<a class=' + '"roomname"' + ' id=' + val.id + ' href="#" value=' + val.id + ' onclick="getRoomDetals(' + val.id + ')"'+'> '+val.name+'</a > ';
+      div.innerHTML += '<a class=' + '"roomname"' + ' id=' + val.id + ' href="#" value=' + val.id + ' onclick="getRoomDetals(' + val.id + ')"' + '> ' + val.name + '</a > ';
       console.log("<li id='" + key + "'>" + val.name + "</li>");
     });
   });
@@ -18,12 +18,23 @@ $(document).ready(function () {
 })
 
 function getRoomDetals(roomid) {
-  $.getJSON('http://localhost:8080/api/rooms/'+roomid, function (data) {
+  $.getJSON('http://localhost:8080/api/rooms/' + roomid, function (data) {
     // var div = document.getElementById('rooms');
-
     $.each(data, function (key, val) {
       // div.innerHTML += '<a class=' + '"roomname"' + ' id=' + val.id + ' href="#" value=' + val.id + ' onclick="getRoomDetals(' + val.id + ')"' > '+val.name+'</a > ';
-      console.log("Key= "+ key+ "Value = "+val);
+      console.log("Key= " + key + " Value = " + val);
+      if (key == 'users') {
+        // console.log("The desired is come.");
+        var i = 0;
+        $.each(val, function (key1, val1) {
+          console.log(i + " " + val1);
+          i++;
+        });
+      }
+      else if (key == 'name') {
+        document.getElementById('roomname').innerHTML = val;
+
+      }
     });
   });
 }
